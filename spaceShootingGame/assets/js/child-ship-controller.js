5d095ca8fc3ec0d69e84447bbea9fea01ff577aa
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-07-25 20:12:56 +0530

    Created redAlarm function in child-ship-controller.js file

--- spaceShootingGame/assets/js/child-ship-controller.js
+++ spaceShootingGame/assets/js/child-ship-controller.js
@@ -4,10 +4,25 @@ setInterval(function () {
             let newTopPX = $("#child-ship" + i).css("top");
             let newTop = parseInt(newTopPX, 10);
             if (newTop>=heightAsNumber){
-                $("#child-ship" + i).remove();
+                redAlarm("#child-ship" + i);
+            }else {
+                newTop++;
+                $("#child-ship" + i).css("top", newTop + 'px');
             }
-            newTop++;
-            $("#child-ship" + i).css("top", newTop + 'px');
+
         }
     }
-}, 1);/*child ship speed change*/
+}, 50);/*child ship speed change*/
+
+function redAlarm(shipID) {
+    $(shipID).remove();
+    setTimeout(function () {
+        $("#level-one").css('height','99vh');
+        $("#level-one").css('boxShadow','0px 5px 13px 6px red');
+    },100);
+    setTimeout(function () {
+        $("#level-one").css('height','100vh');
+        $("#level-one").css('boxShadow','none');
+    },500);
+
+}
