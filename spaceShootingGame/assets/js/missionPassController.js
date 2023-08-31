26530503f49c2ed789a8d342fbacaef8dbe9665c
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-08-31 17:31:49 +0530

    The calculator was created to calculate the next level number

--- spaceShootingGame/assets/js/missionPassController.js
+++ spaceShootingGame/assets/js/missionPassController.js
@@ -1,4 +1,7 @@
 function missionPass() {
+    $("#level-number").text(Number($("#level-number").text()) + 1);
+
+
     document.getElementById("status").style.display = "flex";
     document.getElementById("mission-fail").style.display = "none";
     document.getElementById("mission-pass").style.display = "flex";
