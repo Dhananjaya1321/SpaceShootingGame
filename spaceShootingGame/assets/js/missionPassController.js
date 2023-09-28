5b030d27d0fa117811cf891353c51d83eed8108e
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-09-28 15:22:47 +0530

    Updated calculateScore function

--- spaceShootingGame/assets/js/missionPassController.js
+++ spaceShootingGame/assets/js/missionPassController.js
@@ -5,18 +5,15 @@ function missionPass() {
 }
 
 function calculateScore(level) {
-    switch (level){
-        //10 points were added for blowing up one plane and Deduct 2 points per bullet
-        case 1:
-            //the first level has 10 planes
-            let score=100-countIds*2;
-            if (score>60){
-                $("#score").text(score);
-                $("#level-number").text(level + 1);
-                return true;
-            }else {
-                return false;
-            }
+    //10 points were added for blowing up one plane and Deduct 2 points per bullet
+    let currentLevel =Number($("#level-number").text());
+    let score = 100 * currentLevel - countIds * 2;
+    if (score > (100 * currentLevel) * (6.0 / 10.0)) {
+        $("#score").text(score);
+        $("#level-number").text(currentLevel + 1);
+        return true;
+    } else {
+        return false;
     }
 }
 
@@ -24,13 +21,21 @@ $("#continue-btn").click(function () {
     document.getElementById("status").style.display = "none";
     document.getElementById("mission-fail").style.display = "none";
     document.getElementById("mission-pass").style.display = "none";
-    let level = $("#level-number").text();
+    let level = Number($("#level-number").text());
     let objectCount;
-    switch (level){
-        case 2:objectCount=20;break;
-        case 3:objectCount=30;break;
-        case 4:objectCount=40;break;
-        default:objectCount=10;break;
+    switch (level) {
+        case 2:
+            objectCount = 20;
+            break;
+        case 3:
+            objectCount = 30;
+            break;
+        case 4:
+            objectCount = 40;
+            break;
+        default:
+            objectCount = 10;
+            break;
     }
 
     refreshChildShipPositions(level);
@@ -39,9 +44,9 @@ $("#continue-btn").click(function () {
 })
 
 function removeBullets() {
-    for (let i = 1; i <=countIds; i++) {
-        $("#bullet"+i).remove();
+    for (let i = 1; i <= countIds; i++) {
+        $("#bullet" + i).remove();
     }
-    countIds=0;
-    clickCount=0;
+    countIds = 0;
+    clickCount = 0;
 }//this method use to remove all bullets
