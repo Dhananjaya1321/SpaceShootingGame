65cc999077b7b316face2714494021a880bda22c
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-09-30 23:54:47 +0530

    Updated missionFailController.js

--- assets/js/missionFailController.js
+++ assets/js/missionFailController.js
@@ -3,13 +3,15 @@ function missionFail() {
     document.getElementById("mission-fail").style.display = "flex";
     document.getElementById("mission-pass").style.display = "none";
 }
+let currentLevel;
 
 $("#mission-fail>button:nth-child(3)").click(function () {
     document.getElementById("status").style.display = "none";
     document.getElementById("level-one").style.display = "none";
     document.getElementById("home").style.display = "flex";
-    createChildObjects(10);
-    refreshChildShipPositions(1,true);
+    currentLevel=Number($("#level-number").text());
+    createChildObjects((currentLevel+1)*5);
+    refreshChildShipPositions(currentLevel,true);
     value = false;
     countIds = 0;
     clickCount = 0;
@@ -19,8 +21,9 @@ $("#mission-fail>button:nth-child(2)").click(function () {
     document.getElementById("status").style.display = "none";
     document.getElementById("level-one").style.display = "flex";
     document.getElementById("home").style.display = "none";
-    createChildObjects(10);
-    refreshChildShipPositions(1,true);
+    currentLevel=Number($("#level-number").text());
+    createChildObjects((currentLevel+1)*5);
+    refreshChildShipPositions(currentLevel,true);
     clickCount = 0;
     countIds = 0;
 });
